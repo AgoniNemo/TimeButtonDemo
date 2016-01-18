b5169ed8d0326ce0fdad25bc0b7db6ee5f471d05
Nemo <XZC168520@163.com>
2016-01-18 23:18:58 +0800

    Initial Commit

---
+++ TimeButtonDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeButtonDemo
+//
+//  Created by Nemo on 16/1/18.
+//  Copyright © 2016年 Nemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimeButtonDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimeButtonDemo
+//
+//  Created by Nemo on 16/1/18.
+//  Copyright © 2016年 Nemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
